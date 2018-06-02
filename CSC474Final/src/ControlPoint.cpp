bbd7763a468a5e9f4fbba9fdbc8f27d5727a6786
Chanelle <ckaith97@yahoo.com>
2018-06-02 04:20:53 -0700

    testing commit on VS

--- CSC474Final/src/ControlPoint.cpp
+++ CSC474Final/src/ControlPoint.cpp
@@ -20,38 +20,40 @@ void ControlPoint::loadPoints(string filename) {
 
 	// read file and store points
 	float i, j, k;
-	
-	string strr;
-	//getline(file, strr);
-	cout << "str to path name: " << strr << endl;
 
-
-	mat3 pt = mat3(1.0);
 	//while (file.peek() != EOF) {
 	try {
-		while (getline(file, strr)) {
+
+		string temp;
+		
+		int c = 0;
+
+		string line; 
+		mat3 pt = mat3(1.0);
+		while ((c = file.peek()) != EOF) {
+			if (c == '\n') getline(file, line);		// if new line, skip it;
 			for (int i = 0; i < 3; i++) {
 				vec3 xBase;
+				getline(file, line, ' ');
+				xBase.x = stof(line);		// x
+				getline(file, line, ' ');
+				xBase.y = stof(line);		// y
+				getline(file, line, ' ');
+				xBase.z = stof(line);		// z
 
-				getline(file, strr, ' ');
-				xBase.x = stof(strr);		// x
-				getline(file, strr, ' ');
-				xBase.y = stof(strr);		// y
-				getline(file, strr, ' ');
-				xBase.z = stof(strr);		// z
-				cout << to_string(xBase) << endl;
-
+				cout << "xBase: " << to_string(xBase) << endl;
 				pt[i] = xBase;
 			}
 			points.push_back(pt);
 		}
+
 	}
 	catch (exception e) {
 		cout << "ERROR: File format must be: \n <text>" << endl;
 	}
 
 
-
+	cout << "printing out points" << endl;
 	for (int i = 0; i < points.size(); i++) {
 		cout << to_string(points[i]) << endl;
 	}
